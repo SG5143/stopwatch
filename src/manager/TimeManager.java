6195f0cb65a1fd9a87e671f09291f73caa04b11b
SG5143 <sun5143a@gmail.com>
2024-11-13 14:03:34 +0900

    Add TimeManager

--- src/manager/TimeManager.java
+++ src/manager/TimeManager.java
@@ -1,14 +1,18 @@
 package manager;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-public class TimeManager {
+public class TimeManager implements Runnable {
 	private final TimeZone TIME_ZONE;
 	private final SimpleDateFormat SDF;
-	
+
 	private Calendar calendar;
+
+	private long startTime;
+	private boolean isRun = true;
 
 	private TimeManager() {
 		this.TIME_ZONE = TimeZone.getTimeZone("Asia/Seoul");
@@ -21,12 +25,37 @@ public class TimeManager {
 		return instance;
 	}
 
+	private void initStartTime() {
+		this.startTime = getCurrentTimeMs();
+	}
+
 	public long getCurrentTimeMs() {
 		return System.currentTimeMillis();
 	}
-	
+
 	public String getDateTimeFormat() {
 		this.calendar = Calendar.getInstance(TIME_ZONE);
 		return SDF.format(calendar.getTime());
+	}
+
+	@Override
+	public void run() {
+		initStartTime();
+
+		while (isRun) {
+			IOManager.buffer.append(getDateTimeFormat());
+			IOManager.buffer.append(getCurrentTimeMs() - startTime);
+
+			try {
+				IOManager.writer.append(IOManager.buffer);
+				IOManager.writer.flush();
+
+				Thread.sleep(1000);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }

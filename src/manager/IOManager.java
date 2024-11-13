3b995ea207b47e5d73aa8c5feaf034e5a84596bd
SG5143 <sun5143a@gmail.com>
2024-11-13 15:53:18 +0900

    Change input handling location

--- src/manager/IOManager.java
+++ src/manager/IOManager.java
@@ -2,22 +2,31 @@ package manager;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import stopwatch.Stopwatch;
+
 public class IOManager implements Runnable {
+
+	public static final int START = 1;
+	public static final int STOP = 2;
+	public static final int RESET = 3;
 
 	public static StringBuilder buffer;
 	public static BufferedWriter writer;
 	private BufferedReader reader;
 
+	private Stopwatch stopwatch;
+
+	private int selectMenu;
 	private boolean isRun = true;
 
 	private IOManager() {
 		IOManager.buffer = new StringBuilder();
 		IOManager.writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		this.reader = new BufferedReader(new InputStreamReader(System.in));
+		stopwatch = Stopwatch.getInstance();
 	}
 
 	private static IOManager instance = new IOManager();
@@ -26,34 +35,42 @@ public class IOManager implements Runnable {
 		return instance;
 	}
 
-	public void menu() throws Exception {
-		buffer.append(" << 스톱워치 >> \n");
-		buffer.append("[1] START\n");
-		buffer.append("[2] STOP\n");
-		buffer.append("[3] RESET\n");
+	private void menu() {
+		try {
+			buffer.append(" << 스톱워치 >> \n");
+			buffer.append("[1] START\n");
+			buffer.append("[2] STOP\n");
+			buffer.append("[3] RESET\n");
 
-		writer.append(buffer);
-		writer.flush();
+			writer.append(buffer);
+			buffer.setLength(0);
+			writer.flush();
+
+		} catch (Exception e) {
+		}
 	}
 
-	public void input() {
+	private void input() {
 		try {
 			String input = reader.readLine();
 			int number = -1;
 			number = Integer.parseInt(input);
-			if (number >= 1 && number <= 3) {
-				
-			}
+			if (number >= 1 && number <= 3)
+				selectMenu = number;
 		} catch (Exception e) {
-			e.printStackTrace();
 		}
-
 	}
 
 	@Override
 	public void run() {
+		menu();
 		while (isRun) {
 			input();
+			switch (selectMenu) {
+			case START -> stopwatch.start();
+			case STOP -> stopwatch.stop();
+			case RESET -> stopwatch.reset();
+			}
 		}
 	}
 }

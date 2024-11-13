a5579ec56d1f2825c24bb988dd23a783be0cc179
SG5143 <sun5143a@gmail.com>
2024-11-13 15:57:10 +0900

    src/stopwatch/Stopwatch.java

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -1,9 +1,21 @@
 package stopwatch;
 
+import manager.IOManager;
+import manager.TimeManager;
+
 public class Stopwatch {
 
-	private Stopwatch() {
+	private IOManager ioManager;
+	private TimeManager timeManager;
 
+	private Thread ioManagerT;
+	private Thread timeManagerT;
+
+	private Stopwatch() {
+		this.ioManager = IOManager.getInstance();
+		this.timeManager = TimeManager.getInstance();
+		this.ioManagerT = new Thread(ioManager);
+		this.timeManagerT = new Thread(timeManager);
 	}
 
 	private static Stopwatch instance = new Stopwatch();
@@ -13,6 +25,36 @@ public class Stopwatch {
 	}
 
 	public void run() {
-
+		ioManagerT.start();
+		timeManagerT.start();
 	}
+
+	public void start() {
+		if (timeManager.getElapsedTime() == 0 && (timeManagerT == null || !timeManagerT.isAlive())) {
+			timeManager.start();
+		} else {
+			timeManager.start();
+		}
+	}
+
+	public void stop() {
+		if (timeManager.getStartTime() != 0) {
+			timeManager.stop();
+			if (timeManagerT != null && timeManagerT.isAlive()) {
+				timeManagerT.interrupt();
+			}
+		}
+	}
+
+	public void reset() {
+		if (timeManager.getStartTime() != 0) {
+			timeManager.resetTime();
+			timeManager.stop();
+			if (timeManagerT != null && timeManagerT.isAlive()) {
+				timeManagerT.interrupt();
+				timeManagerT = null;
+			}
+		}
+	}
+
 }
